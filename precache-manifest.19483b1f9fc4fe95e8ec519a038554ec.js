e621d47efcc57176afe4b829acd1f9a27d9ebbe6
LEESOHEE <vkfksvkfks21@naver.com>
2022-12-18 19:43:49 +0900

    Updates

---
+++ precache-manifest.19483b1f9fc4fe95e8ec519a038554ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e6f0e59c609b4f90d7839ee771a8535",
+    "url": "/socon01/index.html"
+  },
+  {
+    "revision": "b8921438dfa41f90cad5",
+    "url": "/socon01/static/css/main.a2a697ff.chunk.css"
+  },
+  {
+    "revision": "3c2d0936b4fe4f6e7c75",
+    "url": "/socon01/static/js/2.227d1eca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/socon01/static/js/2.227d1eca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8921438dfa41f90cad5",
+    "url": "/socon01/static/js/main.24600ee1.chunk.js"
+  },
+  {
+    "revision": "a73e008e4c4f9bdc509a",
+    "url": "/socon01/static/js/runtime-main.634b62a3.js"
+  }
+]);
